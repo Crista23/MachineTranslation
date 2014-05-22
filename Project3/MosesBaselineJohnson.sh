4483e215f71892cae19e10f72b2e1ec0b336555e
Cristina Garbacea <cr1st1na.garbacea@gmail.com>
2014-05-23 00:40:44 +0200

    report

--- Project3/MosesBaselineJohnson.sh
+++ Project3/MosesBaselineJohnson.sh
@@ -19,11 +19,11 @@ ROOT=/home/sveldhoen/MTProject3/$EXPERIMENT
 GIZADIR=$ROOT/giza #/source-target
 MODELDIR=$ROOT/model #/source-target
 CORPUSDIR=$ROOT/corpus #/source-target
-ALIGNEDCORPUS=/home/sveldhoen/MTProject3/data/corpusAligned
+ALIGNEDCORPUS=/home/sveldhoen/MTProject3/data/corpusMiniTest/corpusAligned
+TRANSLATIONDIR=$ROOT/TranslationResults
 
 TARGET=en
-SOURCES=(da)
-#SOURCES=(da de es fr it nl pt)
+SOURCES=(da de es fr it nl pt)
 
 for SOURCE in ${SOURCES[*]}
 do
@@ -36,14 +36,23 @@ do
 	-mgiza -mgiza-cpus 4 \
 	--f $SOURCE --e $TARGET \
 	--lm 0:3:/home/bart/project2_data/lm/europarl-v7.nl-en.train.blm.en:8  \
-	--corpus $ALIGNEDCORPUS -alignment grow-diag-final-and -reordering msd-bidirectional-fe
+	--corpus $ALIGNEDCORPUS -alignment grow-diag-final-and -reordering msd-bidirectional-fe \
 	--root-dir $ROOT \
-	--lexical-dir $MODELDIR/$source-$target          \
-	--model-dir $MODELDIR/$source-$target            \
+	--model-dir $MODELDIR/$SOURCE-$TARGET  \
 	--extract-file $MODELDIR/$SOURCE-$TARGET/extract \
 	--giza-f2e $GIZADIR/$SOURCE-$TARGET \
 	--giza-e2f $GIZADIR/$TARGET-$SOURCE \
+	--first-step 3 \
+	--last-step 9
 	
+	echo "Evaluation begins"
+	/apps/smt_tools/decoders/mosesdecoder/bin/moses \
+	-f "$MODELDIR/$SOURCE-$TARGET/moses.ini" 				#"$rootdir/MosesBaseline/$source/model/moses.ini" \ 	 			#/home/ggarbacea/SSLP/MosesResults/model/moses.ini \
+	< "$CORPUS/test/corpusAligned.$SOURCE"   				#"$testCorpus/p2_test.$source" \
+	> "$TRANSLATIONDIR/translated$SOURCE-$TARGET.$TARGET"   #"$translationLocation/translatedtest$source-to$target.$target" \ 	#/home/ggarbacea/SSLP/TranslationResults/translatedtestNLtoENG.en \
+	2> "$TRANSLATIONDIR/translation$SOURCE-$TARGET.out"
+	
+	echo "Compute Bleu scores"
 	#ask Moses to translate
 	#/apps/smt_tools/decoders/mosesdecoder/bin/moses \
 	#-f "$rootdir/MosesBaseline/$source/model/moses.ini" \ 	 			#/home/ggarbacea/SSLP/MosesResults/model/moses.ini \
