e31675367e50aade1bfed82345d0b294e3beb889
Veldhoen <sara.veldhoen+github@gmail.com>
2014-05-26 19:22:21 +0200

    phrase extraction stuff

--- Project3/extendedPhraseExtraction.py
+++ Project3/extendedPhraseExtraction.py
@@ -1,38 +1,38 @@
-
+import sys
 
 def main():
-# $ELINE $FLINES $ALLINES
-
-    eline = sysargv[0]
-    eline = eline.split()
-    flines = sys.argv[1]
-    flines = flines.split(',')
+# $ELINE $FLINES $ALLINES $OUTPUT
+#    print str(sys.argv)
+    size = 7
+    eline = sys.argv[1].strip().split()
+    print "eline is "+str(eline)
+    flines = sys.argv[2].strip().split('@@')
+#    print "flines is "+str(flines)
     flines = [fline.split() for fline in flines]
-    allines = sys.argv[2]
-    allines = allines.split(',')
-    allines = [alline.split() for alline in allines]
-    output = sys.argv[3]
-    extractPhrases(eLine, fLines, alLines, size)
+    allines = sys.argv[3].strip().split(',')
+#    print "allines is "+str(allines)
+    output = sys.argv[4]
+#    print "output is "+output
+    print "Going to extract phrases"
+#    print eline, flines, allines
+    extractPhrases(eline, flines, allines, size, output)
 
-    extractAllPhrases(eFile, fFiles, alFiles, size)
 
-
-
-def getForeignWindow(e_s,e_e,alignment):
+def getForeignWindow(e_s,e_e, A):
     #initialize f_s and f_e the extreme values
-    f_s = len(alignment)
+    f_s = len(A)
     f_e = 0
     # Loop over the relevant part of the alignment matrix to find positive cells
     # Use the f-coordinate as f_s or f_e if it is smaller/ larger than the value so far
     for i in range(e_s, e_e):
-        for j in range(len(f)):
+        for j in range(len(A)):
             if A[j][i] != 0:
                 f_s = min(j, f_s)
                 f_e = max(j, f_e)
     if f_e >= f_s:
        # Check for consistency of the window pair:
        for j in range(f_s, f_e):
-           for i in range(len(e)):
+           for i in range(len(A[0])):
                if A[j][i]!=0 and (i<e_s or i>e_e):
                #Inconsistent: an alignment point outside of the english window
                #              but inside the foreign window
@@ -40,7 +40,7 @@ def getForeignWindow(e_s,e_e,alignment):
                    f_e = -1
     return f_s,f_e
 
-def extractPhrases(eLine, fLines, alLines, size):
+def extractPhrases(eLine, fLines, alLines, size, phraseTable):
     e = eLine
     f = fLines[0]
     if size <1:
@@ -50,8 +50,9 @@ def extractPhrases(eLine, fLines, alLines, size):
     el = len(e)
     alignments = []
     for i in range(len(fLines)): #iterate over the foreign languages and create alignment matrices
-        fl = len(fLine[i])
-        alignments.append(getAlginmentMatrix(feLines[i],efLines[i],el,fl))
+        print fLines[i]
+        fl = len(fLines[i])
+        alignments.append(getAlignmentMatrix(alLines[i],el,fl))
     A = alignments[0]
 
 
@@ -87,6 +88,7 @@ def extractPhrases(eLine, fLines, alLines, size):
                                       alignment += ' '+str(i)+'-'+str(j)
                            #write to the phrase table (append)
                            with open(phraseTable, "a") as myfile:
+                               print "Extracted a phrase pair"
                                myfile.write(engPhrase+ ' ||| '+forPhrase+' |||'+alignment+'\n')
                                # phrase table file format:
                                #<source phrase> ||| <target phrase> ||| <alignment points>
@@ -100,7 +102,7 @@ def getPhrase(start,end,sentence):
     return phrase[0:-1]  # Omit the last space (" ")
 
 
-def windowOK(e_s, e_e, alignments)
+def windowOK(e_s, e_e, alignments):
     ok = True
     for al in alignments: #iterate over the other foreign languages
         f_s,f_e = getForeignWindow(e_s,e_e,al)
@@ -111,9 +113,19 @@ def windowOK(e_s, e_e, alignments)
     return ok
 
 def getAlignmentMatrix(alLine, el, fl):
+    print "get matrix for "+alLine
+    print "el is",el, "fl is", fl
+
     points = alLine.split()
     alignment = [[0 for col in range(el)] for row in range(fl)]
     for point in points:
-        f_pos, e_pos = point.split("-")
+	print "point is "+point
+        e_pos, f_pos = point.split("-")
+	f_pos = int(f_pos)
+	e_pos = int(e_pos)
         alignment[f_pos][e_pos] = 1
     return alignment
+
+
+if __name__ == "__main__":
+    main()
--- Project3/extractAndComputeCoverage.sh
+++ Project3/extractAndComputeCoverage.sh
@@ -2,7 +2,8 @@
 CORPUS=/home/sveldhoen/MTProject3/data/corpusMiniTest/training
 
 TARGET=en
-SOURCES=(da de fr it nl pt)
+SOURCES=(de fr it nl pt)
+#SOURCES=(da de es fr it nl pt)
 FOCUS=fr
 MIX=(it nl)
 SAME=(it pt)
@@ -15,7 +16,7 @@ KINDS=(union intersect grow-diag-final)
 EXPERIMENT=mini
 
 ROOT=/home/sveldhoen/MTProject3/$EXPERIMENT
-COVERAGE=$ROOT/COVERAGE
+COVERAGE=$ROOT/coverage
 
 mkdir -p $COVERAGE/corpus
 mkdir -p $COVERAGE/alignments
@@ -29,7 +30,7 @@ bash splitCorpus.sh $COVERAGE/corpus heldout
 # And accordingly split the aignment files
 for SOURCE in ${SOURCES[*]};
 do
-  for KIND int ${KINDS[*]};
+  for KIND in ${KINDS[*]};
   do
     cp $ROOT/model/$SOURCE-$TARGET/aligned.$KIND $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND
   done
@@ -39,19 +40,24 @@ bash splitCorpus.sh $COVERAGE/alignments heldout
 
 
 echo 'EXPERIMENT: union/ intersection/ grow-diag-final'
-for $KIND in ${KINDS[*]};
+for KIND in ${KINDS[*]};
 do
-  for i in $(seq 1 50);
+  for i in $(seq 0 2);
   do
-    ELINE=$(head -i $COVERAGE/corpus/training/corpusAligned.$TARGET | tail -1)
-    FLINES=$(head -i $COVERAGE/corpus/training/corpusAligned.$FOCUS | tail -1)
-    ALLINES=$(head -i $COVERAGE/alignments/$FOCUS-$TARGET.alignedg.grow-diag-final | tail -1)
+    ELINE=$(head -$i $COVERAGE/corpus/training/corpusAligned.$TARGET | tail -1)
+    FLINES=$(head -$i $COVERAGE/corpus/training/corpusAligned.$FOCUS | tail -1)
+    ALLINES=$(head -$i $COVERAGE/alignments/$FOCUS-$TARGET.aligned.grow-diag-final | tail -1)
     for SOURCE in ${MIX[*]};
     do
-      FLINES+=,$(head -i $COVERAGE/corpus/training/corpusAligned.$SOURCE | tail -1)
-      ALLINES+=,$(head -i $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND | tail -1)
+      FLINES+=@@$(head -$i $COVERAGE/corpus/training/corpusAligned.$SOURCE | tail -1)
+      ALLINES+=,$(head -$i $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND | tail -1)
     done
-    python extendedPhraseExtraction $ELINE $FLINES $ALLINES $COVERAGE/phrasetables/phrases.fr-en.$KIND.mix
+#    echo "eline is $ELINE\n fLines is $FLINES \n alLines is $ALLINES"
+    python extendedPhraseExtraction.py \
+ 	"$ELINE" \
+	"$FLINES" \
+	"$ALLINES" \ 
+	$COVERAGE/phrasetables/phrases.fr-en.$KIND.mix
   done
 done
 
