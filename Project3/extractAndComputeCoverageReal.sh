a7b2071dc8ee35a825077c12ac3db033e9147a10
Cristina Garbacea <cr1st1na.garbacea@gmail.com>
2014-06-02 13:04:19 +0200

    extract and compute coverage

--- Project3/extractAndComputeCoverageReal.sh
+++ Project3/extractAndComputeCoverageReal.sh
@@ -45,11 +45,12 @@ do
 	echo 'EXPERIMENT: union/ intersection/ grow-diag-final'
 	LENGTH=$(cat $COVERAGE/corpus/$LOCATION/corpusAligned.$TARGET|wc -l)
 	for KIND in ${KINDS[*]};
-	#KIND=union
 	do
 		for i in $(seq 0 $LENGTH);
 		do
 			echo "line $i"
+			#mix
+			echo "Mix"
 			ELINE=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$TARGET | tail -1)
 			FLINES=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$FOCUS | tail -1)
 			ALLINES=$(head -$i $COVERAGE/alignments/$FOCUS-$TARGET.aligned.grow-diag-final | tail -1)
@@ -63,7 +64,58 @@ do
 			"$ELINE" \
 			"$FLINES" \
 			"$ALLINES" \
-			"$COVERAGE/phrasetables/phrases.fr-en.$KIND.mix"
+			"$COVERAGE/phrasetables/phrases.fr-en.$KIND-$LOCATION.mix"
+			
+			#same
+			echo "Same"
+			SELINE=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$TARGET | tail -1)
+			SFLINES=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$FOCUS | tail -1)
+			SALLINES=$(head -$i $COVERAGE/alignments/$FOCUS-$TARGET.aligned.grow-diag-final | tail -1)
+			for SOURCE in ${SAME[*]};
+			do
+			  SFLINES+=@@$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$SOURCE | tail -1)
+			  SALLINES+=,$(head -$i $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND | tail -1)
+			done
+			echo "eline is | $SELINE | ### fLines is | $SFLINES | ### alLines is | $SALLINES |"
+			python extendedPhraseExtraction.py \
+			"$SELINE" \
+			"$SFLINES" \
+			"$SALLINES" \
+			"$COVERAGE/phrasetables/phrases.fr-en.$KIND-$LOCATION.same"
+			
+			#diff
+			echo "Diff"
+			DELINE=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$TARGET | tail -1)
+			DFLINES=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$FOCUS | tail -1)
+			DALLINES=$(head -$i $COVERAGE/alignments/$FOCUS-$TARGET.aligned.grow-diag-final | tail -1)
+			for SOURCE in ${DIFF[*]};
+			do
+			  DFLINES+=@@$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$SOURCE | tail -1)
+			  DALLINES+=,$(head -$i $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND | tail -1)
+			done
+			echo "eline is | $DELINE | ### fLines is | $DFLINES | ### alLines is | $DALLINES |"
+			python extendedPhraseExtraction.py \
+			"$DELINE" \
+			"$DFLINES" \
+			"$DALLINES" \
+			"$COVERAGE/phrasetables/phrases.fr-en.$KIND-$LOCATION.diff"
+			
+			#all
+			echo "All"
+			AELINE=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$TARGET | tail -1)
+			AFLINES=$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$FOCUS | tail -1)
+			AALLINES=$(head -$i $COVERAGE/alignments/$FOCUS-$TARGET.aligned.grow-diag-final | tail -1)
+			for SOURCE in ${DIFF[*]};
+			do
+			  AFLINES+=@@$(head -$i $COVERAGE/corpus/$LOCATION/corpusAligned.$SOURCE | tail -1)
+			  AALLINES+=,$(head -$i $COVERAGE/alignments/$SOURCE-$TARGET.aligned.$KIND | tail -1)
+			done
+			echo "eline is | $AELINE | ### fLines is | $AFLINES | ### alLines is | $AALLINES |"
+			python extendedPhraseExtraction.py \
+			"$AELINE" \
+			"$AFLINES" \
+			"$AALLINES" \
+			"$COVERAGE/phrasetables/phrases.fr-en.$KIND-$LOCATION.all"
 		done
 	done
 done
